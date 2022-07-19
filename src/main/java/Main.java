b9945629e4b1c3e6702c2e8adf13f11fea1d447a
Abdella <abdella.shukur2@gmail.com>
2022-07-19 08:50:29 -0400

    got all values greater than 25

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
-import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         int[] numbers = { 1, 4, 5, 2, 34, 36, 5, 4, 23, 45, 6, 8, 56, 34, 37 };
-        ArrayList<Integer> res = getNums(numbers);
+        TreeSet<Integer> res = getNums(numbers);
         System.out.println(res); // [34, 36, 37, 45, 56]
     }
 
-    public static ArrayList<Integer> getNums(int[] nums) {
-        // your code here
-        return null;
+    public static TreeSet<Integer> getNums(int[] nums) {
+       // Find all numbers greater than 25 (Tailset)
+
+        TreeSet<Integer> quarterSet = new TreeSet<>();
+        quarterSet.add(1);   quarterSet.add(5);     quarterSet.add(56);
+        quarterSet.add(4);   quarterSet.add(4);     quarterSet.add(34);
+        quarterSet.add(5);   quarterSet.add(23);    quarterSet.add(37);
+        quarterSet.add(2);   quarterSet.add(45);
+        quarterSet.add(34);   quarterSet.add(6);
+        quarterSet.add(36);   quarterSet.add(8);
+
+        quarterSet = (TreeSet<Integer>) (quarterSet.tailSet(25));
+
+        return quarterSet;
     }
 }
